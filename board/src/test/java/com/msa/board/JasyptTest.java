856d791c3d8e22fc00dc97c02840e5e52f690641
JinAru16 <wjdcksdnr11@naver.com>
2025-03-17 16:41:25 +0900

    jwt.secret-key를 해석 못하는 상황

---
+++ board/src/test/java/com/msa/board/JasyptTest.java
@@ -0,0 +1,27 @@
+package com.msa.board;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.iv.NoIvGenerator;
+import org.jasypt.salt.RandomSaltGenerator;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+public class JasyptTest {
+    @Value("${jasypt.encryptor.password}")
+    String password;
+
+
+    @Test
+    public void testEncrypt() {
+
+       ;
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        encryptor.setPassword(password);
+        encryptor.setSaltGenerator(new RandomSaltGenerator());
+        encryptor.setIvGenerator(new NoIvGenerator());
+
+        String decrypted = encryptor.decrypt("pLkCYqJR4z4GDQ+hTBA/8X+6EwlSMAqk7ZY5WvrBw6nbVVdyWzTihZNAACBY6H6pcfQ02ErreLEhCkY1wR/3wEEAWVK0KAdGGDB0/jmP+wPu3u288RPktDAlSkunjzLquxUXTjJq1KyDPKPtuY+zSNtXsv1F0PwBe+yZe07DuCw+RUQa+RqtqBnLkobyy1JLPTL/5dqxaKg/+HhX3IOeIw==");
+        System.out.println("복호화 결과: " + decrypted);
+    }
+}
