3b44a896161e21a18d96f037ef2bae005d92e93a
JinAru16 <wjdcksdnr11@naver.com>
2025-04-07 12:49:04 +0900

    test profile

--- board/src/test/java/com/msa/board/JasyptTest.java
+++ board/src/test/java/com/msa/board/JasyptTest.java
@@ -8,8 +8,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
 @ActiveProfiles("test")
+@SpringBootTest
 public class JasyptTest {
 
 }
