220cc8667f73ede1e1346720e7a5a1e89c7f0c69
JinAru16 <wjdcksdnr11@naver.com>
2025-05-08 10:48:57 +0900

    테스트코드  복원

---
+++ board/src/test/java/com.msa.board/community/reply/service/ReplyServiceTest.java
@@ -0,0 +1,77 @@
+package com.msa.board.community.reply.service;
+
+import com.msa.board.common.exception.BoardException;
+import com.msa.board.community.domain.Entity.Community;
+import com.msa.board.community.reply.domain.entity.Reply;
+import com.msa.board.community.reply.domain.request.ReplyRequest;
+import com.msa.board.community.reply.repository.ReplyRepository;
+import com.msa.board.community.repository.CommunityRepository;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ActiveProfiles("test")
+@SpringBootTest
+@Transactional
+class ReplyServiceTest {
+    @Autowired
+    ReplyRepository replyRepository;
+
+    @Autowired
+    CommunityRepository communityRepository;
+
+    @BeforeEach
+    void setUp() {
+        // id가 3인 게시글에 댓글을 미리 셋팅함.
+        Community communityForReply = communityRepository.findById(3L).orElseThrow(() -> new BoardException("Community not found"));
+        // given
+        List<ReplyRequest> requestList = new ArrayList<>();
+        ReplyRequest request1 = new ReplyRequest(3L, "test reply", "test user");
+        ReplyRequest request2 = new ReplyRequest(3L, "댓글2", "admin");
+        ReplyRequest request3 = new ReplyRequest(3L, "댓글 조회기능", "사용자1");
+        ReplyRequest request4 = new ReplyRequest(3L, "게시글 조회시 한꺼번에 댓글 조회", "익명 사용자");
+        ReplyRequest request5 = new ReplyRequest(3L, "게시글과 댓글 조회", "바코드닉");
+
+        requestList.add(request1);
+        requestList.add(request2);
+        requestList.add(request3);
+        requestList.add(request4);
+        requestList.add(request5);
+        Reply reply = new Reply();
+        List<Reply> list = requestList.stream().map(s -> reply.createReply(s, communityForReply)).toList();
+
+        replyRepository.saveAll(list);
+    }
+
+
+
+    @Test
+    void addReply() {
+        // given
+        ReplyRequest request = new ReplyRequest(1L, "test reply", "test user");
+
+        // when
+        Community communityForReply = communityRepository.findById(1L).orElseThrow(() -> new BoardException("Community not found"));
+
+        Reply reply = new Reply();
+        Reply newReply = reply.createReply(request, communityForReply);
+        Reply save = replyRepository.save(newReply);
+
+        // then
+        System.out.println("community_id : " + save.getCommunity().getId());
+        System.out.println("reply id : " + save.getId());
+        System.out.println("reply content : " + save.getReplyContent());
+        assertThat(save.getId()).isNotNull();
+        assertThat(save.getReplyContent()).isEqualTo("test reply");
+
+    }
+}
